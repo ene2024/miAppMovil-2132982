ff26af99ef8d97a5ec4f704e98269cf6fd224bc6
joseenriquez1404 <manuel.enriquezr@uanl.edu.mx>
2024-03-20 18:30:52 -0600

    Enlace de propiedades

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,4 +17,6 @@ export class HomePage {
     alert("Hiciste clic");
   }
 
+  imgUrl: string = 'https://th.bing.com/th/id/R.e40a6a639b2839d751b9f710295ac7d7?rik=r%2fFTDfhgKQ3NkA&riu=http%3a%2f%2fcodinginfinite.com%2fwp-content%2fuploads%2f2019%2f05%2fmaxresdefault-1.jpg&ehk=RkNcCbLHYXXIf%2fm6EOAE%2fBOsFurN6iFpgtga58vKS4o%3d&risl=&pid=ImgRaw&r=0';
+
 }
